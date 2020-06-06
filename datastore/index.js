84f3829b03a8634cd058183e8e036485a6871a64
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-05 18:38:07 -0700

    Complete deleting a todo

--- datastore/index.js
+++ datastore/index.js
@@ -55,19 +55,17 @@ exports.update = (id, text, callback) => {
       });
     }
   });
-
-
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  let filePath = path.join(exports.dataDir, id + '.txt');
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      callback(new Error(`No item with id: ${id}`));
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
